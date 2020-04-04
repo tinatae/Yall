232eec8c355af609d2f4d35b4cf6e839e10caba9
tinatae <tinakt3@gmail.com>
2020-04-04 15:49:51 -0700

    Adjust another migration

--- db/migrate/20200106044445_change_hours_in_businesses.rb
+++ db/migrate/20200106044445_change_hours_in_businesses.rb
@@ -1,33 +1,47 @@
 class ChangeHoursInBusinesses < ActiveRecord::Migration[5.2]
   def change
-    change_column :businesses, :monopen, :string, null: false
-    change_column :businesses, :monclose, :string, null: false
-    remove_column :businesses, :monopenend, :string, null: false
-    remove_column :businesses, :moncloseend, :string, null: false
-    change_column :businesses, :tuesopen, :string, null: false
-    change_column :businesses, :tuesclose, :string, null: false
-    remove_column :businesses, :tuesopenend, :string, null: false
-    remove_column :businesses, :tuescloseend, :string, null: false
-    change_column :businesses, :wedopen, :string, null: false
-    change_column :businesses, :wedclose, :string, null: false
-    remove_column :businesses, :wedopenend, :string, null: false
-    remove_column :businesses, :wedcloseend, :string, null: false
-    change_column :businesses, :thursopen, :string, null: false
-    change_column :businesses, :thursclose, :string, null: false
-    remove_column :businesses, :thursopenend, :string, null: false
-    remove_column :businesses, :thurscloseend, :string, null: false
-    change_column :businesses, :friopen, :string, null: false
-    change_column :businesses, :friclose, :string, null: false
-    remove_column :businesses, :friopenend, :string, null: false
-    remove_column :businesses, :fricloseend, :string, null: false
-    change_column :businesses, :satopen, :string, null: false
-    change_column :businesses, :satclose, :string, null: false
-    remove_column :businesses, :satopenend, :string, null: false
-    remove_column :businesses, :satcloseend, :string, null: false
-    change_column :businesses, :sunopen, :string, null: false
-    change_column :businesses, :sunclose, :string, null: false
-    remove_column :businesses, :sunopenend, :string, null: false
-    remove_column :businesses, :suncloseend, :string, null: false
+    # change_column :businesses, :monopen, :string, null: false
+    # change_column :businesses, :monclose, :string, null: false
+
+    # remove_column :businesses, :monopenend, :string, null: false
+    # remove_column :businesses, :moncloseend, :string, null: false
+
+    # change_column :businesses, :tuesopen, :string, null: false
+    # change_column :businesses, :tuesclose, :string, null: false
+
+    # remove_column :businesses, :tuesopenend, :string, null: false
+    # remove_column :businesses, :tuescloseend, :string, null: false
+
+    # change_column :businesses, :wedopen, :string, null: false
+    # change_column :businesses, :wedclose, :string, null: false
+
+    # remove_column :businesses, :wedopenend, :string, null: false
+    # remove_column :businesses, :wedcloseend, :string, null: false
+
+    # change_column :businesses, :thursopen, :string, null: false
+    # change_column :businesses, :thursclose, :string, null: false
+
+    # remove_column :businesses, :thursopenend, :string, null: false
+    # remove_column :businesses, :thurscloseend, :string, null: false
+
+    # change_column :businesses, :friopen, :string, null: false
+    # change_column :businesses, :friclose, :string, null: false
+
+    # remove_column :businesses, :friopenend, :string, null: false
+    # remove_column :businesses, :fricloseend, :string, null: false
+
+    # change_column :businesses, :satopen, :string, null: false
+    # change_column :businesses, :satclose, :string, null: false
+
+    # remove_column :businesses, :satopenend, :string, null: false
+    # remove_column :businesses, :satcloseend, :string, null: false
+
+    # change_column :businesses, :sunopen, :string, null: false
+    # change_column :businesses, :sunclose, :string, null: false
+
+    # remove_column :businesses, :sunopenend, :string, null: false
+    # remove_column :businesses, :suncloseend, :string, null: false
+
     remove_column :businesses, :opennow, :string, default: "Always"
  
     add_index :businesses, :monopen
